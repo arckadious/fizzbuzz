671b534ea522534dbae433a70e4ea8be8db5a977
Pierre PERRIER-RIDET <pierre.perrierridet@groupepvcp.com>
2023-04-26 19:39:49 +0200

    add container.go and database.go tests

--- container/container_test.go
+++ container/container_test.go
@@ -1,1 +1,23 @@
 package container
+
+import (
+	"testing"
+
+	"github.com/arckadious/fizzbuzz/config"
+	"github.com/arckadious/fizzbuzz/validator"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+
+	cf, err := config.New("../parameters/parameters.json", *validator.New())
+	assert.Equal(t, nil, err)
+	container := New(cf, validator.New())
+	assert.NotEqual(t, nil, container)
+	assert.NotEqual(t, nil, container.Conf)
+	assert.NotEqual(t, nil, container.Validator)
+	assert.NotEqual(t, nil, container.FizzAction)
+	assert.NotEqual(t, nil, container.Repo)
+	assert.NotEqual(t, nil, container.RepoFizz)
+}
--- database/database_test.go
+++ database/database_test.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"testing"
+
+	"github.com/arckadious/fizzbuzz/config"
+	"github.com/arckadious/fizzbuzz/validator"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+
+	cf, err := config.New("../parameters/parameters.json", *validator.New())
+	assert.Equal(t, nil, err)
+	New(cf)
+
+}
